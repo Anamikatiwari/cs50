3d615d0fd18a72b8533607465196b41aac498138
Anamika Tiwari <155213271+Anamikatiwari@users.noreply.github.com>
2024-03-24 19:01:03 +0545

    day4, added hello,name, conditions,sequences,list,sets in python

---
+++ lists.py
@@ -0,0 +1,8 @@
+#Define a list of names
+names = ["Anamika", "Harry", "Shyam", "Ram", "Sita"]
+
+names.append("Ginny")
+
+names.sort()
+
+print(names)
---
+++ sequences.py
@@ -0,0 +1,11 @@
+
+# name = "abcdefghijklmnopqrstuvwxyz"
+
+
+# name = ["Anamika", "Harry", "Shyam", "Ram", "Sita"]
+# print(name[1])
+
+
+coordinateX = 10.0
+coordinateX = 20.0
+cooedinate = (10.0, 20.0)
